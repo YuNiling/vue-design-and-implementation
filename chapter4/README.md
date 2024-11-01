455e2527760fa678822e36890251caac213b86a2
YN Wang <ynwang@YNdeMacBook-Air.local>
2024-11-01 16:27:35 +0800

    chapter4 计算属性computed与lazy

--- chapter4/README.md
+++ chapter4/README.md
@@ -7,7 +7,7 @@
 - [x] 嵌套的 effect 与 effect 栈
 - [x] 避免无限递归循环
 - [x] 调度执行
-- [ ] 计算属性 computed 与 lazy
+- [x] 计算属性 computed 与 lazy
 - [ ] watch 的实现原理
 - [ ] 立即执行的 watch 与回调执行时机
 - [ ] 过期的副作用
---
+++ chapter4/computed9.js
@@ -0,0 +1,158 @@
+// * 计算属性：通过 lazy 让副作用函数不立即执行，然后通过手动方式让其执行，实现计算属性的设计。
+
+// 用一个全局变量存储被注册的副作用函数
+let activeEffect;
+// effect 栈
+const effectStack = [];
+// 存储副作用函数的桶
+const bucket = new WeakMap();
+// 原始数据
+const data = {
+  foo: 1,
+  bar: 2
+};
+// 对原始数据的代理
+const obj = new Proxy(data, {
+  get(target, key) {
+    track(target, key);
+    return target[key]
+  },
+  set(target, key, newVal) {
+    target[key] = newVal;
+    trigger(target, key);
+    return true;
+  }
+});
+
+let temp1, temp2;
+
+function track(target, key) {
+  if (!activeEffect) return;
+  let depsMap = bucket.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    bucket.set(target, depsMap);
+  }
+  let effectsSet = depsMap.get(key);
+  if (!effectsSet) {
+    effectsSet = new Set();
+    depsMap.set(key, effectsSet);
+  }
+  effectsSet.add(activeEffect);
+  activeEffect.deps.push(effectsSet);
+}
+
+function trigger(target, key) {
+  const depsMap = bucket.get(target);
+  if (!depsMap) return;
+  const effectsSet = depsMap.get(key);
+  const effectsToRun = new Set();
+  effectsSet.forEach((effectFn => {
+    if (effectFn !== activeEffect) {
+      effectsToRun.add(effectFn);
+    }
+  }));
+  effectsToRun.forEach(effectFn => {
+    // 如果一个副作用函数存在调度器，则调用该调度器，并将副作用函数作为参数传递
+    // 负责直接执行副作用函数（之前到默认行为）
+    if (effectFn.options.scheduler) {
+      effectFn.options.scheduler(effectFn);
+    } else {
+      effectFn();
+    }
+  });
+}
+
+// 副作用函数
+function effect(fn, options = {}) {
+  const effectFn = () => {
+    cleanup(effectFn);
+    activeEffect = effectFn;
+    effectStack.push(effectFn);
+    const res = fn();
+    effectStack.pop();
+    activeEffect = effectStack[effectStack.length - 1];
+    return res;
+  };
+  // 将 options 挂载到 effectFn 上
+  effectFn.options = options;
+  // activeEffect.deps 用来存储所有与该副作用相关联的依赖集合
+  effectFn.deps = [];
+  // 只有非 lazy 的时候，才执行
+  if (!options.lazy) {
+    effectFn();
+  }
+  // 将副作用函数作为返回值返回
+  return effectFn;
+}
+
+function cleanup(effectFn) {
+  effectFn.deps.forEach((deps) => {
+    deps.delete(effectFn);
+  });
+
+  // 重置数组
+  effectFn.deps = [];
+}
+
+// 定义一个任务队列，采用 Set 数据结构目的是自动去重
+const jobQueue = new Set();
+// 使用 Promise.resolve() 创建一个 promise 实例，我们用它将一个任务添加到微任务队列
+const p = Promise.resolve();
+
+// 一个标志代表是否正在刷新队列
+let isFlushing = false;
+function flushJob() {
+  // 如果队列正在刷新，则什么都不做
+  if (isFlushing) return;
+  // 设置为 true ，代表正在刷新
+  isFlushing = true;
+  // 在微任务队列中刷新 jobQueue 队列
+  p.then(() => {
+    jobQueue.forEach(job => job());
+  }).finally(() => {
+    // 结束后重置 isFlushing
+    isFlushing = false;
+  });
+}
+
+// 执行副作用函数，触发读取
+function computed(getter) {
+  // value 用来换成上一次计算的值
+  let value;
+  // dirty 标志，用来标识是否需要重新计算值，为 true 则意味着“脏”，需要计算
+  let dirty = true;
+
+  // 把 getter 作为副作用函数，创建一个 lazy 的 effect
+  const effectFn = effect(getter, {
+    lazy: true,
+    scheduler() {
+      dirty = true;
+      // 当计算属性依赖的响应式数据变化时，手动调用 trigger 函数触发响应
+      trigger(obj, 'value');
+    }
+  });
+  const obj = {
+    // 当读取 value 时才执行 effectFn()
+    get value() {
+      // 只有“脏”时才计算值，并将得到的值缓存到 value 中
+      if (dirty) {
+        value = effectFn();
+        dirty = false;
+      }
+      // 当读取 vaue 时，手动调用 track 函数进行追踪
+      track(obj, 'value');
+      return value;
+    }
+  };
+
+  return obj;
+}
+
+const sumRes = computed(() => obj.foo + obj.bar);
+
+effect(() => {
+  console.log(sumRes.value);
+});
+
+obj.foo++;
