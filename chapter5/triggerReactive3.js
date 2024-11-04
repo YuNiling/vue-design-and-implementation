8dd2a7d67e398feb1252dc5c910350e346622d62
YN Wang <ynwang@YNdeMacBook-Air.local>
2024-11-04 12:35:31 +0800

    chapter5 合理的触发响应

---
+++ chapter5/triggerReactive3.js
@@ -0,0 +1,311 @@
+// * 如何合理触发响应：
+// * 1.修改值但值没有变化，不需要触发响应（包括边界值：NaN）；
+// * 2.修改 child 不存在反而原型 parent 上存在的属性值，只需要触发 child 代理对象的 set 拦截函数，屏蔽 parent 代理对象的 set 拦截函数。
+
+// 用一个全局变量存储被注册的副作用函数
+let activeEffect;
+// effect 栈
+const effectStack = [];
+// 存储副作用函数的桶
+const bucket = new WeakMap();
+const ITERATE_KEY = Symbol();
+const TriggerType = {
+  SET: 'SET',
+  ADD: 'ADD',
+  DELETE: 'DELETE'
+};
+
+// 对原始数据的代理
+function reactive(obj) {
+  return new Proxy(obj, {
+    get(target, key, receiver) {
+      // 代理对象可以通过 raw 属性访问原始数据
+      if (key === 'raw') {
+        return target;
+      }
+
+      track(target, key);
+      return Reflect.get(target, key, receiver);
+    },
+    set(target, key, newVal, receiver) {
+      // 先获取旧值
+      const oldVal = target[key];
+
+      // 如果属性不存在，则说明是在添加新属性，负责是设置已有属性
+      const type = Object.prototype.hasOwnProperty.call(target, key) ? TriggerType.SET : TriggerType.ADD;
+      const res = Reflect.set(target, key, newVal, receiver);
+      // target === receiver.raw 说明 receiver 就是 target 的代理对象
+      if (target === receiver.raw) {
+        // 比较新值与旧值，只有当不全等，并且都不是 NaN 的时候才触发响应
+        if (oldVal !== newVal && (oldVal === oldVal || newVal === newVal)) {
+          trigger(target, key, type);
+        }
+      }
+
+      return res;
+    },
+    deleteProperty(target, key) {
+      // 检查被操作的属性是否是对象自己的属性
+      const hadKey = Object.prototype.hasOwnProperty.call(target, key);
+      // 使用 Reflect.deleteProperty 完成属性的删除
+      const res = Reflect.deleteProperty(target, key);
+
+      if (res && hadKey) {
+        // 只有当被删除的属性是对象自己的属性并且成功删除时，才触发更新
+        trigger(target, key, TriggerType.DELETE);
+      }
+
+      return res;
+    },
+    has(target, key) {
+      track(target, key);
+      return Reflect.has(target, key);
+    },
+    ownKeys(target) {
+      // 将副作用函数与 ITERATE_KEY 管理
+      track(target, ITERATE_KEY);
+      return Reflect.ownKeys(target);
+    }
+  });
+}
+
+let temp1, temp2;
+
+function track(target, key) {
+  if (!activeEffect) return;
+  let depsMap = bucket.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    bucket.set(target, depsMap);
+  }
+  let effectsSet = depsMap.get(key);
+  if (!effectsSet) {
+    effectsSet = new Set();
+    depsMap.set(key, effectsSet);
+  }
+  effectsSet.add(activeEffect);
+  activeEffect.deps.push(effectsSet);
+}
+
+function trigger(target, key, type) {
+  const depsMap = bucket.get(target);
+  if (!depsMap) return;
+  const effectsSet = depsMap.get(key);
+
+
+  const effectsToRun = new Set();
+  effectsSet && effectsSet.forEach((effectFn => {
+    if (effectFn !== activeEffect) {
+      effectsToRun.add(effectFn);
+    }
+  }));
+
+  // 只有当操作类型为 ‘ADD’ 或 'DELETE' 时，才触发与 ITERATE_KEY 相关联的副作用函数重新执行
+  if (type === TriggerType.ADD || type === TriggerType.DELETE) {
+    // 取得与 ITERATE_KEY 相关联的副作用函数
+    const iterateEffects = depsMap.get(ITERATE_KEY);
+
+    // 将与 ITERATE_KEY 相关联的副作用函数也添加到 effectsToRun
+    iterateEffects && iterateEffects.forEach(effectFn => {
+      if (effectFn !== activeEffect) {
+        effectsToRun.add(effectFn);
+      }
+    });
+  }
+
+  effectsToRun.forEach(effectFn => {
+    // 如果一个副作用函数存在调度器，则调用该调度器，并将副作用函数作为参数传递
+    // 负责直接执行副作用函数（之前到默认行为）
+    if (effectFn.options.scheduler) {
+      effectFn.options.scheduler(effectFn);
+    } else {
+      effectFn();
+    }
+  });
+}
+
+// 副作用函数
+function effect(fn, options = {}) {
+  const effectFn = () => {
+    cleanup(effectFn);
+    activeEffect = effectFn;
+    effectStack.push(effectFn);
+    const res = fn();
+    effectStack.pop();
+    activeEffect = effectStack[effectStack.length - 1];
+    return res;
+  };
+  // 将 options 挂载到 effectFn 上
+  effectFn.options = options;
+  // activeEffect.deps 用来存储所有与该副作用相关联的依赖集合
+  effectFn.deps = [];
+  // 只有非 lazy 的时候，才执行
+  if (!options.lazy) {
+    effectFn();
+  }
+  // 将副作用函数作为返回值返回
+  return effectFn;
+}
+
+function cleanup(effectFn) {
+  effectFn.deps.forEach((deps) => {
+    deps.delete(effectFn);
+  });
+
+  // 重置数组
+  effectFn.deps = [];
+}
+
+// 定义一个任务队列，采用 Set 数据结构目的是自动去重
+const jobQueue = new Set();
+// 使用 Promise.resolve() 创建一个 promise 实例，我们用它将一个任务添加到微任务队列
+const p = Promise.resolve();
+
+// 一个标志代表是否正在刷新队列
+let isFlushing = false;
+function flushJob() {
+  // 如果队列正在刷新，则什么都不做
+  if (isFlushing) return;
+  // 设置为 true ，代表正在刷新
+  isFlushing = true;
+  // 在微任务队列中刷新 jobQueue 队列
+  p.then(() => {
+    jobQueue.forEach(job => job());
+  }).finally(() => {
+    // 结束后重置 isFlushing
+    isFlushing = false;
+  });
+}
+
+// 执行副作用函数，触发读取
+function computed(getter) {
+  // value 用来换成上一次计算的值
+  let value;
+  // dirty 标志，用来标识是否需要重新计算值，为 true 则意味着“脏”，需要计算
+  let dirty = true;
+
+  // 把 getter 作为副作用函数，创建一个 lazy 的 effect
+  const effectFn = effect(getter, {
+    lazy: true,
+    scheduler() {
+      dirty = true;
+      // 当计算属性依赖的响应式数据变化时，手动调用 trigger 函数触发响应
+      trigger(obj, 'value');
+    }
+  });
+  const obj = {
+    // 当读取 value 时才执行 effectFn()
+    get value() {
+      // 只有“脏”时才计算值，并将得到的值缓存到 value 中
+      if (dirty) {
+        value = effectFn();
+        dirty = false;
+      }
+      // 当读取 vaue 时，手动调用 track 函数进行追踪
+      track(obj, 'value');
+      return value;
+    }
+  };
+
+  return obj;
+}
+
+// watch 函数接收两个参数，source 是响应式数据/getter函数，cb 是回调函数
+function watch(source, cb, options = {}) {
+  // 定义 getter
+  let getter;
+  // 如果 source 是函数，说明用户传递的是 getter，所以直接把 source 赋值给 getter
+  if (typeof source === 'function') {
+    getter = source;
+  } else {
+    // 调用 traverse 递归地读取
+    getter = () => traverse(source);
+  }
+
+  let oldVal, newVal;
+
+  // cleanup 用来存储用户注册的过期回调
+  let cleanup;
+  // 定义 onInvalidate 函数
+  function onInvalidate(fn) {
+    cleanup = fn;
+  }
+
+  // 提取 scheduler 调度函数为一个独立的 job 函数
+  const job = () => {
+    // 当数据变化时，调用回调函数 cb
+    newVal = effectFn();
+    // 在调用回调函数 cb 之前，先调用过期回调
+    if (cleanup) {
+      cleanup();
+    }
+    cb(newVal, oldVal, onInvalidate);
+    // 更新旧值，不然下一次会得到错误的旧值
+    oldVal = newVal;
+  };
+
+  const effectFn = effect(
+    () => getter(),
+    {
+      lazy: true,
+      scheduler: () => {
+        // 值调度函数中判断 flush 是否为 'post'，如果是，将其放到微任务队列中执行
+        if (options.flush === 'post') {
+          const p = Promise.resolve();
+          p.then(job);
+        } else {
+          job();
+        }
+      }
+    }
+  );
+
+  if (options.immediate) {
+    job();
+  } else {
+    oldVal = effectFn();
+  }
+}
+
+function traverse(value, seen = new Set()) {
+  // 如果要读取的数据是原始值，或者已经被读取过了，那么什么也不做
+  if (typeof value !== 'object' || value === null || seen.has(value)) return;
+  // 将数据添加到 seen 中，代表遍历地读取过了，避免循环引用引起的死循环
+  seen.add(value);
+  // 暂时不考虑数组等其他结构
+  // 假设 value 就是一个对象，使用 for...in 读取对象的每一个值，并递归调用 traverse 进行处理
+  for (const k in value) {
+    traverse(value[k], seen);
+  }
+
+  return value;
+}
+
+// 原始数据
+// const data = {
+//   // foo: 1
+//   foo: NaN
+// };
+// const obj = reactive(data);
+// effect(() => {
+//   console.log(obj.foo);
+// });
+// 测试：当值没有发送变化时，是否触发响应
+// obj.foo = 1;
+// 测试：当值为 NaN 时，修改的值为 NaN ，是否触发响应
+// obj.foo = NaN;
+
+const obj = {};
+const proto = { bar: 1 };
+const child = reactive(obj);
+const parent = reactive(proto);
+// 使用 parent 作为 child 的原型
+Object.setPrototypeOf(child, parent);
+effect(() => {
+  console.log(child.bar);
+});
+// 测试：修改 child 不存在反而原型 parent 上存在的属性值，是否只响应一次
+// 原始对象和代理对象判断
+console.log(child.raw === obj);
+console.log(parent.raw === proto);
+child.bar = 2;
