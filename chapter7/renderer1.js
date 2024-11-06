84efb5465cb09cddec15470153dc34e3f99bd15a
YN Wang <ynwang@YNdeMacBook-Air.local>
2024-11-06 18:37:08 +0800

    chapter7 学习完成

---
+++ chapter7/renderer1.js
@@ -0,0 +1,12 @@
+import { effect, ref } from '../reactive.js';
+
+function renderer(domString, container) {
+  container.innerHTML = domString;
+}
+
+// 测试：响应数据值变更，会导致副作用函数重新执行，渲染函数重新调用
+const count = ref(1);
+effect(() => {
+  renderer(`<h1>${count.value}</h1>`, document.getElementById('app'));
+});
+count.value++;
